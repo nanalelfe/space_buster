34bfda710cea5fedc456a85ad01f5f45525fba10
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2016-06-04 21:16:24 -0400

    Made an item bounce on the screen and detect border collision

--- assets/scripts/game_play.js
+++ assets/scripts/game_play.js
@@ -3,37 +3,39 @@ window.onload = function() {
 	var c = document.getElementById("main");
     window.ctx = c.getContext("2d");
 
-    animate();
+    window.x_coord = 40;
+    window.y_coord = 40;
+
+    window.dx = 5;
+    window.dy = 5;
+
+    setInterval(draw,10);
+    //animate();
 
 
 }
 
+
 function draw() {
 	var ctx = window.ctx;
 
-    ctx.beginPath();
-    ctx.arc(150, 150, 75, 0, Math.PI, false);
-    ctx.closePath();
-    //ctx.stroke();
+    ctx.clearRect(0, 0, 1000, 1000);
 
-    var grd = ctx.createLinearGradient(0,0,450,0);
-    grd.addColorStop(0,"pink");
-    grd.addColorStop(1,"turquoise");
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(x_coord, y_coord, 50, 50);
 
-    ctx.fillStyle = grd;
-    ctx.fill();
+    if ( window.x_coord < 0 || window.x_coord > 950) {
+        window.dx = - window.dx;
+    } 
+  
+    if ( window.y_coord < 0 || window.y_coord > 590) {
+        window.dy = - window.dy;
+    } 
+         
+    window.x_coord += window.dx; 
+    window.y_coord += window.dy;
 
-    ctx.fillRect(125, 130, 50, 50); // x, y, w, h
-
-    ctx.beginPath();
-    ctx.moveTo(140, 150);
-    ctx.lineTo(160, 150);
-    ctx.lineTo(150, 170);
-    ctx.lineTo(140, 150);
-    ctx.closePath();
-
-    ctx.strokeStyle = "lightcyan";
-    ctx.stroke();
+    
 }
 
 function animate() {
@@ -43,37 +45,9 @@ function animate() {
     
     // Draw here, including conditionals
     
-    /*ctx.fillStyle = "#000000";
-    ctx.fillRect(x, 40,50, 50);*/
+    
 
     draw();
-
-    /******** DRAW FUNCTION COPY PASTED ********
-
-    ctx.beginPath();
-    ctx.arc(150, 150, 75, 0, Math.PI, false);
-    ctx.closePath();
-    //ctx.stroke();
-    var grd = ctx.createLinearGradient(0,0,450,0);
-    grd.addColorStop(0,"pink");
-    grd.addColorStop(1,"turquoise");
-
-    ctx.fillStyle = grd;
-    ctx.fill();
-
-    ctx.fillRect(125, 130, 50, 50); // x, y, w, h
-
-    ctx.beginPath();
-    ctx.moveTo(140, 150);
-    ctx.lineTo(160, 150);
-    ctx.lineTo(150, 170);
-    ctx.lineTo(140, 150);
-    ctx.closePath();
-
-    ctx.strokeStyle = "lightcyan";
-    ctx.stroke();
-    /****************************************/
-
 
     ctx.translate(3, 2); 
 
